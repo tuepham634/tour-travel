6f2563a627ddc7968fe46c81ee4c53bdbc04aebd
Tuệ Phạm <tuepham634@gmail.com>
2025-03-28 17:40:15 +0700

    Tích hợp pug

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// Thiết lập views
+app.set('views', path.join(__dirname,"views"))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render("client/pages/home",{
+    pageTitle:"Trang chủ"
+  })
 })
 app.get('/tours', (req, res) => {
-    res.send('Trang danh sách tour')
+    res.render("client/pages/tour-list",{
+      pageTitle:"Danh sách tour"
+    })
   })
 app.listen(port, () => {
   console.log(`Website trang chạy trên cổng ${port}`)
