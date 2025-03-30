2ff8fc7cfcd00769dc52ef2f78091b1cbc3ecac4
Tuệ Phạm <tuepham634@gmail.com>
2025-03-30 23:35:28 +0700

    Tách Controller

---
+++ controllers/client/home.controller.js
@@ -0,0 +1,6 @@
+
+module.exports.home = (req, res) => {
+  res.render("client/pages/home",{
+    pageTitle:"Trang chủ"
+  })
+}
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE);
 
-const Tour = require('./models/tour.model');
 
 
+const tourController = require('./controllers/client/tour.controller');
+const homeController = require('./controllers/client/home.controller');
 
 const app = express()
 const port = 3000
@@ -16,19 +17,8 @@ app.set('views', path.join(__dirname,"views"))
 app.set('view engine', 'pug')
 // Thiết lập thư mục chứa file tĩnh của frontend
 app.use(express.static(path.join(__dirname,"public")))
-app.get('/', (req, res) => {
-  res.render("client/pages/home",{
-    pageTitle:"Trang chủ"
-  })
-})
-app.get('/tours',async(req, res) => {
-    const tourList = await Tour.find({});
-    console.log(tourList);
-    res.render("client/pages/tour-list",{
-      pageTitle:"Danh sách tour",
-      tourList:tourList
-    })
-})
+app.get('/', homeController.home);
+app.get('/tours', tourController.list);
 app.listen(port, () => {
   console.log(`Website trang chạy trên cổng ${port}`)
 })
