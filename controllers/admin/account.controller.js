3220fbf69218bc47d2ed9bf4cba81f303f1ca8ba
Tuệ Phạm <tuepham634@gmail.com>
2025-03-31 17:31:29 +0700

    Tích hợp trang  Nhập mã OTP và trang đổi mật khẩu

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -3,8 +3,6 @@ module.exports.login = (req, res) => {
       pageTitle:"Đăng Nhập"
     })
   }
-
-
 module.exports.register = (req, res) => {
     res.render("Admin/pages/register",{
       pageTitle:"Đăng Ký"
@@ -13,5 +11,15 @@ module.exports.register = (req, res) => {
 module.exports.forgotPassword = (req, res) => {
   res.render("Admin/pages/forgot-password",{
     pageTitle:"Quên Mật Khẩu"
+  })
+}
+module.exports.otpPassword = (req, res) => {
+  res.render("Admin/pages/otp-password",{
+    pageTitle:"Nhập Mã OTP"
+  })
+}
+module.exports.resetPassword = (req, res) => {
+  res.render("Admin/pages/reset-password",{
+    pageTitle:"Đổi Mật Khẩu"
   })
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const accountController = require('../../controllers/admin/account.controller');
 
 router.get('/login', accountController.login);
-
 router.get('/register',accountController.register)
-
 router.get('/forgot-password',accountController.forgotPassword)
+router.get('/otp-password',accountController.otpPassword)
+router.get('/reset-password',accountController.resetPassword)
 
 
 module.exports = router;
