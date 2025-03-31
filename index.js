a0572146e790f9158acf3792dd40ac82290d1906
Tuệ Phạm <tuepham634@gmail.com>
2025-03-31 15:47:09 +0700

    Sửa đường dẫn admin linh động

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 require('dotenv').config() //dùng để load các biến môi trường từ file .env vào process.env.
 const adminRoutes = require('./routes/admin/index.route');
 const clientRoutes = require('./routes/client/index.route');
+const variableConfig = require("./config/variable");
 const app = express()
 const port = 3000
 const database = require('./config/database');
@@ -12,8 +13,11 @@ app.set('views', path.join(__dirname,"views"))
 app.set('view engine', 'pug')
 // Thiết lập thư mục chứa file tĩnh của frontend
 app.use(express.static(path.join(__dirname,"public")))
+// Tạo biến toàn cục trong file PUG
+app.locals.pathAdmin = variableConfig.pathAdmin;
+
 //thiết lập đường dẫn
-app.use('/admin', adminRoutes);
+app.use(`/${variableConfig}`, adminRoutes);
 app.use('/', clientRoutes);
 
 app.listen(port, () => {
