2cb961bd354f1c0b278b38546ab22837eb7d5bb1
Tuệ Phạm <tuepham634@gmail.com>
2025-04-07 14:41:16 +0700

    3.2. Tích hợp Joi.dev

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const accountController = require('../../controllers/admin/account.controller');
-
+const accountValidate = require('../../validates/admin/account.validate');
 router.get('/login', accountController.login);
 router.get('/register',accountController.register);
-router.post('/register',accountController.registerPost);
+router.post('/register',accountValidate.registerPost,accountController.registerPost);
 router.get('/register-initial', accountController.registerInitial)
 router.get('/forgot-password',accountController.forgotPassword)
 router.get('/otp-password',accountController.otpPassword)
