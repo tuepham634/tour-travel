c51c6d0182ec9726cf38a2642024aa3750521563
Tuệ Phạm <tuepham634@gmail.com>
2025-05-09 14:23:46 +0700

    1.2. Validate dữ liệu bên backend

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -2,10 +2,16 @@ const router = require('express').Router();
 const multer  = require('multer');
 
 const categoryController = require('../../controllers/admin/category.controller');
+const categoryValidate = require('../../validates/admin/category.validate');
 const cloudinaryHelper = require('../../helpers/cloudinary.helper');
 const upload = multer({storage: cloudinaryHelper.storage});
 router.get('/list',categoryController.list);
 router.get('/create',categoryController.create);
-router.post('/create',upload.single('avatar'),categoryController.createPost);
+router.post(
+    '/create',
+    upload.single('avatar'),
+    categoryValidate.createPost,
+    categoryController.createPost
+);
 
 module.exports = router;
