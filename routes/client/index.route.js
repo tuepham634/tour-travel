0acffd6352786fa260d934c649d085e91b4dcf2c
Tuệ Phạm <tuepham634@gmail.com>
2025-05-22 15:24:59 +0700

    02. Hiển thị data ra Top Header

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,8 @@ const homeRoutes = require('./home.route');
 const tourRoutes = require('./tour.route');
 const cartRoutes = require('./cart.route');
 
+const SettingMiddleware = require("../../middlewares/client/setting.middleware");
+router.use(SettingMiddleware.websiteInfo);
 router.use('/', homeRoutes);
 router.use('/tours', tourRoutes);
 router.use('/cart', cartRoutes);
