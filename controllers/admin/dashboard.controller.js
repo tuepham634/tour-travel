7af0aae74dfdb8868e922e83c8d39f10997d1ffc
Tuệ Phạm <tuepham634@gmail.com>
2025-06-15 19:06:37 +0700

    3.1. Khối Thông số tổng quan

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,4 +1,7 @@
-module.exports.dashboard = (req, res) => {
+const AccountAdmin = require("../../models/account-admin.model");
+const Order = require("../../models/order.model");
+
+module.exports.dashboard = async (req, res) => {
   if (!req.permissions.includes("dashboard-view")){
     res.json({
       code:"error",
@@ -6,7 +9,30 @@ module.exports.dashboard = (req, res) => {
     })
     return;
   }
+  // Section 1
+  const overview = {
+    totalAdmin: 0,
+    totalUser: 0,
+    totalOrder: 0,
+    totalPrice: 0
+  };
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false
+  });
+
+  const orderList = await Order.find({
+    deleted: false
+  })
+
+  overview.totalOrder = orderList.length;
+
+  overview.totalPrice = orderList.reduce((sum, item) => {
+    return sum + item.total;
+  }, 0);
+  // End Section 1
   res.render("Admin/pages/dashboard",{
-    pageTitle:"Tổng Quan"
+    pageTitle:"Tổng Quan",
+    overview:overview
   })
   }
